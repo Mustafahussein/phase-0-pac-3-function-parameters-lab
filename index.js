c35d871c2bbc789bfade8d9e0ae0746c6bf6aa69
Mustafa Hussein <mustafahussein.a@gmail.com>
2023-03-09 03:49:28 -0500

    JavaScript Function Parameeters Lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction('Mus');
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage()
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional();
